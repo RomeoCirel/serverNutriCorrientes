895dead98403bd2ed49f51458c5742d7406b7def
Cirel H. Romeo <cirelhromeo@gmail.com>
2021-12-25 10:48:50 -0300

    Define las rutas para login, logout y logoutAllSesions en api.php

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('auth/login', [AuthController::class, 'login']);
 
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('auth/logout', [AuthController::class, 'logout']);
+    Route::post('auth/logoutgeneral', [AuthController::class, 'logoutAllSessions']);
    
     // Usuarios
     Route::apiResource('usuarios', UsuariosController::class);
