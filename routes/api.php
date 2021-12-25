42b7621b84aafb60ea9625955ccf5b990c485a49
Cirel H. Romeo <cirelhromeo@gmail.com>
2021-12-25 12:01:28 -0300

    Define la ruta para recuperar password en api.php

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::post('auth/login', [AuthController::class, 'login']);
-
+Route::post('auth/recuperar', [PasswordController::class, 'recuperar']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('auth/logout', [AuthController::class, 'logout']);
